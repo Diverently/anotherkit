41561fc89cccb219b78490377a517eedc67eb3c3
Robert Cordes <robert@diverently.com>
2020-12-14 21:59:46 +0100

    fix: Remove unused packages

--- site/config/config.php
+++ site/config/config.php
@@ -1,9 +1,7 @@
 <?php
 
-/**
- * XML Sitemap Options: https://github.com/omz13/kirby3-xmlsitemap#configuration
- * Laravel Mix Kirby Options: https://github.com/Diverently/laravel-mix-kirby#setup
- */
 return [
     'debug' => true,
+    'diverently.laravel-mix-kirby.manifestPath' => 'mix-manifest.json',
+    'diverently.laravel-mix-kirby.assetsDirectory' => '',
 ];
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,17 +2,13 @@ const mix = require("laravel-mix")
 
 mix.setPublicPath("public/assets")
 
-mix.browserSync({
-    host: "anotherkit.test",
-    proxy: "anotherkit.test",
-    open: "external",
-    notify: false,
-    files: ["public/assets/**/*.css", "public/assets/**/*.js", "site/**/*.php", "content/**/*.txt"],
+mix.options({
+    processUrls: false,
 })
 
 mix
-    .js("src/js/main.js", "public/assets/js")
-    .postCss("src/css/main.css", "public/assets/css", [
+    .js("src/js/main.js", "public/js")
+    .postCss("src/css/main.css", "public/css", [
         require("tailwindcss"),
     ])
     .version()
