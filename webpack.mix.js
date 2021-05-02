2c3fb123214daf9c99c90e4d58eacc2bf871c550
Robert Cordes <robert@diverently.com>
2021-05-02 19:03:44 +0200

    fix: Use new processCssUrls option

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,13 +2,12 @@ const mix = require("laravel-mix")
 
 mix.setPublicPath("public")
 
-mix.options({
-    processUrls: false,
-})
-
 mix
     .js("src/js/main.js", "public/js")
     .postCss("src/css/main.css", "public/css", [
         require("tailwindcss"),
     ])
+    .options({
+        processCssUrls: false,
+    })
     .version()
