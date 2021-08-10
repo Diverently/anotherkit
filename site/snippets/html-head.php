3af95b1fae24b8837b57f85015c0c64d96c29b4c
Robert Cordes <robert@diverently.com>
2021-08-10 23:36:26 +0200

    chore: create dark mode favicon

--- site/snippets/html-head.php
+++ site/snippets/html-head.php
@@ -2,6 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <?php echo mix("/css/main.css") ?>
-<link rel="icon" type="image/png" href="/images/favicon.png">
+
 <?php snippet('meta_information'); ?>
 <?php snippet('robots'); ?>
+<link rel="icon" type="image/svg+xml" sizes="any" href="/images/favicon.svg">
+<link rel="icon" type="image/png" href="/images/favicon.png">
