e7be0e454d4378c40b4a5b6b717ed8505657cf54
Robert Cordes <robert@diverently.com>
2021-08-10 23:21:04 +0200

    feat: Use MetaKnight for SEO See https://github.com/diesdasdigital/kirby-meta-knight for more info

--- site/config/config.php
+++ site/config/config.php
@@ -2,6 +2,12 @@
 
 return [
     'debug' => false,
-    'diverently.laravel-mix-kirby.manifestPath' => 'mix-manifest.json',
-    'diverently.laravel-mix-kirby.assetsDirectory' => '',
+    'diverently.laravel-mix-kirby' => [
+        'manifestPath' => 'mix-manifest.json',
+        'assetsDirectory' => '',
+    ],
+    'diesdasdigital.meta-knight' => [
+        'siteTitleAsHomePageTitle' => true,
+        'separator' => ' | ',
+    ],
 ];
--- site/snippets/html-head.php
+++ site/snippets/html-head.php
@@ -1,24 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title><?php echo $browserTitle ?></title>
 <?php echo mix("/css/main.css") ?>
 <link rel="icon" type="image/png" href="/images/favicon.png">
-
-<!-- SEO and Open Graph -->
-<meta name="description" content="">
-<meta property="og:title" content="<?php echo $browserTitle ?>" />
-<meta property="og:type" content="website" />
-<meta property="og:url" content="<?php echo $page->url() ?>" />
-<meta property="og:image" content="" />
-
-<!-- For more information, visit https://schema.org/Organization -->
-<script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@type": "Organization",
-        "name": "<?php echo $site->title() ?>",
-        "url": "<?php echo $site->url() ?>",
-        "image": "",
-    }
-</script>
+<?php snippet('meta_information'); ?>
+<?php snippet('robots'); ?>
