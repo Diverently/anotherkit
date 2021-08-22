61b7700a6771799e1ae021b8d836376aeb1103c6
Robert Cordes <robert@diverently.com>
2021-08-22 21:17:20 +0200

    fix: Initialize Alpine as a module

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -89,7 +89,12 @@
             <div class="text-center">
                 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                     <span class="block xl:inline">Get up and running with</span>
-                    <span class="block text-purple-600 xl:inline">Kirby, Tailwind and Alpine</span>
+                    <span class="block text-purple-600 xl:inline">
+                        <span x-init="setTimeout(() => { $el.classList.add('animate-pulse') }, 500)">Kirby,</span>
+                        <span x-init="setTimeout(() => { $el.classList.add('animate-pulse') }, 1000)">Tailwind</span>
+                        <span x-init="setTimeout(() => { $el.classList.add('animate-pulse') }, 1500)">and</span>
+                        <span x-init="setTimeout(() => { $el.classList.add('animate-pulse') }, 2000)">Alpine</span>
+                </span>
                 </h1>
                 <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-2xl">
                     Anotherkit is brought to you by <a href="https://diverently.com">Diverently</a>. Feel free to contribute to the project on GitHub.
