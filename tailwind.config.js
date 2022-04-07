ca7ababaf22fa7d5899e5041dc0911c0a7982942
Robert Cordes <robert@diverently.com>
2022-04-07 09:18:55 +0200

    feat: Setup Tailwind for quick customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
         './src/js/**/*.js',
@@ -5,12 +8,24 @@ module.exports = {
         './content/**/*.txt',
     ],
     theme: {
+        screens: {
+            'xs': '400px',
+            ...defaultTheme.screens,
+        },
         fontFamily: {
             'sans': ['Lato', 'sans-serif'],
         },
         fontWeight: {
             'normal': 400,
             'bold': 700,
+        },
+        colors: {
+            white: colors.white,
+            black: colors.black,
+            transparent: 'transparent',
+            current: 'currentColor',
+            gray: colors.slate,
+            purple: colors.purple,
         },
         extend: {},
     },
